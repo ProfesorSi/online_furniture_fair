cb040f482a6e76121cc618eb84ca82196894333d
Edis <edis.sijaric95@gmail.com>
2021-05-23 23:35:08 +0200

    fair closed

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app">
-    <Navigation />
+
+ <!-- Comment in to return original fair -->
+   <!-- <Navigation /> -->
     <!-- <span class="go-back">
       <button @click="goBack">Prethodna</button>
     </span> -->
 
-    <router-view></router-view>
+   <!-- <router-view></router-view> -->
+
+   <!-- Furniture is closed -->
+    <div class="closed-fair">
+      <img alt="Sajam logo" src="../src/assets/group102.png" />
+      <div class="article">
+        <p>ZAHVALJUJEMO SE NA 70 HILJADA PREGLEDA U PRETHODNIH SEDAM DANA. </p> <br>
+    <h3>ŽELITE PRIJAVITI IZLAGANJE NA JESENJI SAJAM </h3> <br> PRIJAVE NA <br> 
+          <a href="mailto:INFO@NAMJESTAJ.SHOP"><h2>INFO@NAMJESTAJ.SHOP</h2></a>
+
+          <p>DO OTVARANJA NOVOG SAJMA 
+KUPUJTE NAMJEŠTAJ ONLINE NA <br>
+<a href="https://namjestaj.shop/"><h1>WWW.NAMJESTAJ.SHOP</h1></a></p>
+      </div>
+      <div class="footer">
+        <img alt="Sajam logo" src="../src/assets/logo.png" />
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import Navigation from "./components/Navigation.vue";
+// import Navigation from "./components/Navigation.vue";
 
 export default {
   name: "App",
   components: {
-    Navigation,
   },
 
   methods: {
@@ -28,6 +48,24 @@ export default {
 
 <style>
 @import url("https://fonts.googleapis.com/css?family=Poppins");
+
+.closed-fair {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin: 0;
+  margin-top: 25px;
+}
+
+img {
+  width: 200px;
+  
+}
+
+.footer {
+  margin-top: 50px;
+}
 
 .go-back {
   display: flex;
@@ -43,16 +81,17 @@ export default {
 
 }
 
-#app {
+/* #app {
   width: 100%;
   display: flex;
   flex-direction: row;
+  justify-content: center;
   margin: 0;
   margin-top: 25px;
   font-family: "Poppins";
   font-style: normal;
   font-weight: 600;
-}
+} */
 
 a {
   text-decoration: none;
